fe27b3e561c603921901791e5e05fdbdc70837d0
Darshana Gunawardana <darshana4it@gmail.com>
2020-12-09 23:04:01 +0530

    Portal component

---
+++ src/components/Portal/Portal.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { ClassName } from 'utilities/ClassNames';
+import styles from './Portal.module.scss';
+
+function Portal({ children, className, ...props }) {
+  const [body, setBody] = useState();
+
+  useEffect(() => {
+    if (document) {
+      setBody(document.body);
+    }
+  }, []);
+
+  if (body) {
+    return createPortal(
+      <div className={styles.portal} {...props}>
+        {children}
+      </div>,
+      body
+    );
+  }
+  return null;
+}
+
+export default Portal;
