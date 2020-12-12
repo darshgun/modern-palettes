6395ede1548ae6eb8eb3110e5eb81dd5854ad3e6
Darshana Gunawardana <darshana4it@gmail.com>
2020-12-12 22:43:50 +0530

    Sync color inputs and Implement shadow urls

--- src/pages/name-that-color/[hex]/index.js
+++ src/pages/name-that-color/[hex]/index.js
@@ -1,7 +1,15 @@
+import React from 'react';
 import { useRouter } from 'next/router';
+import NameThatColor from 'pages/name-that-color';
+import validateHex, { returnIfNotColor } from 'helpers/validateHex';
 
-export default function Page() {
+export default function index() {
   const router = useRouter();
+  const { hex } = router.query;
+  const color = `#${hex}`;
 
-  return <span>Click me</span>;
+  if (validateHex(color)) {
+    return <NameThatColor urlColor={color} />;
+  }
+  return <h1>Invalid color code</h1>;
 }
--- src/pages/name-that-color/index.js
+++ src/pages/name-that-color/index.js
@@ -1,45 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { SeoTags } from 'helpers/seoTags';
 import Header from 'layouts/Header';
 import nameColor from 'helpers/nameColor';
+import validateHex, { returnIfNotColor } from 'helpers/validateHex';
 import { Container, Grid, Heading, Card, ColorBoard } from 'components';
+import styles from './NameColor.module.scss';
 
-export default function index() {
-  const [color, setColor] = useState('#fefefe');
+export default function index({ urlColor }) {
   const router = useRouter();
-  const { query } = router;
+
+  const [color, setColor] = useState(urlColor);
+  const [colorInput, setColorInput] = useState(urlColor);
+  const [typing, setTyping] = useState(false);
 
   /**
    * ColorPicker actions
    */
   const handleColorBoardOnChange = ({ hex }) => {
     setColor(hex);
+    setColorInput(hex);
   };
   const handleColorBoardOnChangeComplete = ({ hex }) => {
-    setColor(hex);
+    setShadowUrl(hex);
   };
+
+  const setShadowUrl = (urlSegment) => {
+    const urlSegmentWithoutHash = urlSegment.replace('#', '');
+    setTimeout(() => {
+      router.push(`/name-that-color/${urlSegmentWithoutHash}`, undefined, { shallow: true });
+    });
+  };
+
+  useEffect(() => {
+    if (validateHex(colorInput)) {
+      setColor(colorInput);
+      if (typing) {
+        setShadowUrl(colorInput);
+      }
+    }
+  }, [colorInput]);
 
   return (
     <div>
       <Header />
       <SeoTags title="Name that color" description="Find the name of a color" />
-      <Heading title="Name that color" subtitle="More Tools" />
+      <Heading title="Enter the color" subtitle="Name that color">
+        <input
+          className={styles.colorInput}
+          type="text"
+          value={colorInput}
+          onFocus={() => setTyping(true)}
+          onBlur={() => setTyping(false)}
+          onChange={(e) => setColorInput(e.target.value)}
+        />
+      </Heading>
       <Container>
         <Grid>
           <Grid.Column size={{ sm: '1/3' }}>
             <ColorBoard
+              elementClassName={styles.colorBoardHeight}
               color={color}
               onChange={handleColorBoardOnChange}
               onChangeComplete={handleColorBoardOnChangeComplete}
             />
           </Grid.Column>
-          <Grid.Column size={{ sm: '2/3' }}>
-            Color code: {color} <br />
-            Color name: {nameColor(color)}
-          </Grid.Column>
+          <Grid.Column size={{ sm: '2/3' }}>Color name: {nameColor(color)}</Grid.Column>
         </Grid>
       </Container>
     </div>
   );
 }
+
+index.defaultProps = {
+  urlColor: '#fefefe',
+};
